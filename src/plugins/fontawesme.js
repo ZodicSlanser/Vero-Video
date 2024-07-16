e6274108a3c1ddbe254abbdbc0c59b614e145fa1
moniazaki <131484014+moniazaki@users.noreply.github.com>
2024-07-16 16:25:13 +0300

    Delete src directory

--- src/plugins/fontawesme.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
